44e73050d92245155ad34d2dab5512d33fef037a
Satoki <43191933+satokiy@users.noreply.github.com>
2023-12-31 11:29:55 +0900

    monorepo

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "path";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
